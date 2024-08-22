12b80c926debb7bfbe765e986bd2f7fd0c01f915
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2024-08-21 20:49:47 -0700

    delete old dumping data and code

--- OLD/dump/combinecrit.py
+++
@@ -1,5 +0,0 @@
-SIZE = 88
-with open("data/crit.db", "wb+") as out_file: 
-    for i in range(SIZE):
-        with open(f"parts/crit{i}.db", "rb") as db_file:
-            out_file.write(db_file.read())
